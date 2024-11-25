f80fc6fb40f41e8b82f58d570ace96ad07ffaeb6
ecesanal <elifecesanal4@gmail.com>
2024-11-25 18:17:16 +0300

    first commit

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductImageDtos/UpdateProductImageDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Catalog.Dtos.ProductImageDtos
+{
+    public class UpdateProductImageDto
+    {
+        public string ProductImageId { get; set; }
+        public string Images1 { get; set; }
+        public string Images2 { get; set; }
+        public string Images3 { get; set; }
+
+        public string ProductId { get; set; }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class ProductImage
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductImageId { get; set; }
+        public string Images1 { get; set; }
+        public string Images2 { get; set; }
+        public string Images3 { get; set; }
+
+        public string ProductId { get; set; }
+
+        [BsonIgnore]
+        public Product Product { get; set; }
+    }
+}
