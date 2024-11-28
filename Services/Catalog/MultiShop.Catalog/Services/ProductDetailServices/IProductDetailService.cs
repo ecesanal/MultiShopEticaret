ea391035a20ab5a95809a0d776cb4b3e250f6c6d
ecesanal <elifecesanal4@gmail.com>
2024-11-28 20:36:53 +0300

    services

---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductDetailServices/IProductDetailService.cs
@@ -0,0 +1,15 @@
+﻿using MultiShop.Catalog.Dtos.ProductDetailDtos;
+using MultiShop.Catalog.Dtos.ProductDtos;
+
+namespace MultiShop.Catalog.Services.ProductDetailServices
+{
+    public interface IProductDetailService
+    {
+
+        Task<List<ResultProductDetailDto>> GetAllProductAsync();
+        Task CreateProductDetailAsync(CreateProductDetailDto ProductDetailDto);
+        Task UpdateProductDetailAsync(UpdateProductDetailDto updateProductDetailDto);
+        Task DeleteProductDetailAsync(string id);
+        Task<GetByIdProductDetailDto> GetByIdProductDetailAsync(string id);
+    }
+}
