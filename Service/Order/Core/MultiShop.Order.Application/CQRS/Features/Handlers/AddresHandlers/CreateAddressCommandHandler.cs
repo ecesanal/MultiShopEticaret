3cf34bbd112f90714b295bb847201b844fbf6a83
ecesanal <elifecesanal4@gmail.com>
2024-12-07 00:07:37 +0300

    CQRS

---
+++ Service/Order/Core/MultiShop.Order.Application/CQRS/Features/Handlers/AddresHandlers/CreateAddressCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MultiShop.Order.Application.CQRS.Features.Commands.AddresCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.CQRS.Features.Handlers.AddresHandlers
+{
+    public class CreateAddressCommandHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public CreateAddressCommandHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(CreateAddressCommand createAddressCommand)
+        {
+            await _repository.CreateAsync(new Address
+            {
+                City = createAddressCommand.City,
+                Detail = createAddressCommand.Detail,
+                District = createAddressCommand.District,
+                UserId = createAddressCommand.UserId
+
+            });
+        }
+    }
+}
