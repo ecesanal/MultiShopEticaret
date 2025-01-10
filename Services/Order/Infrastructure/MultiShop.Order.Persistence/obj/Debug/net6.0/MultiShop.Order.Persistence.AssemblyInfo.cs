3f9f12b842a5e635051c03fc96e327b4f88f86c9
ecesanal <elifecesanal4@gmail.com>
2025-01-10 19:35:18 +0300

    pagination UI

--- Services/Order/Infrastructure/MultiShop.Order.Persistence/obj/Debug/net6.0/MultiShop.Order.Persistence.AssemblyInfo.cs
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/obj/Debug/net6.0/MultiShop.Order.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Order.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5de99887a3fb00756008f2617ce4ba5bab9fe3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+382fffc5c4327183849c10eb4825f7231132370d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Order.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Order.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
